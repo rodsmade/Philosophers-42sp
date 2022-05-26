a4611773da88440e9e1c66f51e46a1510e072614
rodsmade <rodrigogregoldo@gmail.com>
2022-05-26 00:10:55 -0300

    basic project structure ok, Makefile ok

---
+++ philo/include/philo.h
@@ -0,0 +1,12 @@
+#ifndef PHILO_H
+# define PHILO_H
+
+// ---------------------------------------------------------	INCLUDES
+# include <pthread.h>	// pthread_create, pthread_join, pthread_mutex_init etc.
+# include <stdio.h>		// printf
+# include <string.h>	// memset
+# include <stdlib.h>	// malloc, free
+# include <unistd.h>	// write, usleep, 
+# include <sys/time.h>	// gettimeofday
+
+#endif
---
+++ philo/src/philosophers.c
@@ -0,0 +1,7 @@
+#include "philo.h"
+
+int main()
+{
+	printf("hello world\n");
+	return (0);
+}
