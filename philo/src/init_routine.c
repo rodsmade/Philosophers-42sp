a7bdbae6102b53c81fac11201bff3e5d66563537
gustavdlima <gustavdlima@gmail.com>
2022-08-11 12:13:25 -0300

    add: each philosopher have a pointer to the next fork

--- philo/src/init_routine.c
+++ philo/src/init_routine.c
@@ -17,12 +17,6 @@ void	init_philosophers(t_philosopher **philosophers, t_args *args)
 	while (++i < args->number_of_philosophers)
 		pthread_mutex_init((*philosophers)[i].this_fork, NULL);
 	i = -1;
-	// while (++i < args->number_of_philosophers)
-	// {
-	// 	(*philosophers)[i].next_fork = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
-	// 	if (i == (args->number_of_philosophers - 1))
-	// 		(*philosophers)[i].next_fork = &((*philosophers)[0].this_fork);
-	// 	else
-	// 		(*philosophers)[i].next_fork = &((*philosophers)[i + 1].this_fork);
-	// }
+	while (++i < args->number_of_philosophers)
+		(*philosophers)[i].next_fork = ((*philosophers)[(i + 1) % args->number_of_philosophers].this_fork);
 }
