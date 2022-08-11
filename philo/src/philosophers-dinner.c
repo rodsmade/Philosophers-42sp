4026bcb98328f2037053fa7cad5d239d54980da0
rodsmade <rodrigogregoldo@gmail.com>
2022-08-11 11:38:56 -0300

    two forks implemented with no leaks

--- philo/src/philosophers-dinner.c
+++ philo/src/philosophers-dinner.c
@@ -34,6 +34,7 @@ void	destroy_philosophers(t_philosopher **philosophers, t_args *args)
 	{
 		free((*philosophers)[i].thread);
 		pthread_mutex_destroy((*philosophers)[i].this_fork);
+		free((*philosophers)[i].this_fork);
 	}
 	free(*philosophers);
 }
